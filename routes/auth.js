a2a6a4d1f01eb86c9cf6ad61376cb124c85abf97
Vyas Giri <vyasgiri25@gmail.com>
2023-09-21 03:35:54 +0530

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+// @desc Auth with Google
+// @route GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc Google auth callback
+// @route GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', { failureRedirect:'/' }), (req, res) => {
+    res.redirect('/dashboard')
+})
+
+// @desc Logout user
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout(req.user, err => {
+        if (err) return next(err);
+        res.redirect('/')
+    });
+});
+
+
+module.exports = router
